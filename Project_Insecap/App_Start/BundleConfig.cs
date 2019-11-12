2c1a70a3d32af90b01960b5c21118a4a4b281407
rcvillegas1987 <57239884+rcvillegas1987@users.noreply.github.com>
2019-11-12 16:17:52 -0300

    Carga Template Bootstrap

---
+++ Project_Insecap/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Project_Insecap.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles) {
+            /*
+
+             */
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/vendors/ti-icons/css/themify-icons.css",
+                "~/Content/vendors/css/vendor.bundle.base.css",
+                "~/Content/css/vertical-layout-light/style.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include("~/Content/vendors/js/vendor.bundle.base.js",
+                        "~/Content/vendors/chart.js/Chart.min.js'",
+                        "~/Content/js/off-canvas.js'",
+                        "~/Content/js/hoverable-collapse.js'",
+                        "~/Content/js/template.js'",
+                        "~/Content/js/settings.js'",
+                        "~/Content/js/todolist.js' ",
+                        "~/Content/js/dashboard.js'"));
+
+        }
+    }
+}
--- Project_Insecap/Global.asax.cs
+++ Project_Insecap/Global.asax.cs
@@ -1,8 +1,10 @@
+using Project_Insecap.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Project_Insecap
@@ -13,6 +15,7 @@ namespace Project_Insecap
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
